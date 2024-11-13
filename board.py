2de52b42dbbd39a86ddf45a190a77e7e65447011
Falanger-debug <falanger27@gmail.com>
2024-11-13 13:02:29 +0100

    Working

--- board.py
+++ board.py
@@ -1,14 +1,41 @@
 import tkinter as tk
 from tkinter import filedialog, ttk
+
 import numpy as np
 
-from settings import *
+# Game settings
+WIDTH, HEIGHT = 150, 100
+CELL_SIZE = 10
+MIN_CELL_SIZE = 2
+MAX_CELL_SIZE = 50
+FPS = 10
+FULLSCREEN = True
 
-running = False
+# Default directory for file dialog
+INITIAL_DIR = "./data_points"
 
+# B: Birth, S: Survival, [Number of Neighbors]
+RULES = [
+    {"name": "GAME_OF_LIFE","B": [3],"S": [2, 3]},
+    {"name": "SEEDS","B": [2],"S": []},
+    {"name": "LIFE_WITHOUT_DEATH","B": [3],"S": [0, 1, 2, 3, 4, 5, 6, 7, 8]},
+    {"name": "CORAL","B": [3],"S": [4, 5, 6, 7, 8]},
+    {"name": "AMOEBA","B": [3, 5, 7],"S": [1, 3, 5, 8]},
+    {"name": "DAY_AND_NIGHT","B": [3, 6, 7, 8],"S": [3, 4, 6, 7, 8]},
+    {"name": "LONG_LIFE","B": [3, 4, 5],"S": [5]}
+]
+RULE = RULES[0]
 
+# Colors
+BUTTON_COLOR = "#6272a4"
+BUTTON_HOVER_COLOR = "#bd93f9"
+GRID_COLOR = "#44475a"
+LIVE_CELL_COLOR = "#50fa7b"
+BACKGROUND_COLOR = "#282a36"
 
+# Grid data
 grid = np.zeros((WIDTH, HEIGHT), dtype=int)
+running = False
 
 
 class GameOfLifeApp:
@@ -81,32 +108,7 @@ class GameOfLifeApp:
         # Game loop
         self.draw_grid()
 
-    def count_neighbors(self,x, y):
-        neighbors = 0
-        for i in range(-1, 2):
-            if x + i < 0 or x + i >= WIDTH:
-                continue
-            for j in range(-1, 2):
-                if y + j < 0 or y + j >= HEIGHT:
-                    continue
-                if i == 0 and j == 0:
-                    continue
-                neighbors += grid[x + i, y + j]
-        return neighbors
-
-    def update_grid(self):
-        global grid
-        new_grid = np.copy(grid)
-        for x in range(WIDTH):
-            for y in range(HEIGHT):
-                neighbors = self.count_neighbors(x, y)
-                if grid[x, y] == 1 and (neighbors not in RULE["S"]):
-                    new_grid[x, y] = 0
-                elif grid[x, y] == 0 and neighbors in RULE["B"]:
-                    new_grid[x, y] = 1
-        grid = new_grid
-
-    def resize_canvas(self):
+    def resize_canvas(self, event=None):
         self.canvas.config(width=self.root.winfo_width() - 20, height=self.root.winfo_height() - 160)
         self.draw_grid()
 
@@ -130,6 +132,31 @@ class GameOfLifeApp:
         RULE = next(rule for rule in RULES if rule["name"] == selected_rule)
         self.draw_grid()
 
+
+    def count_neighbors(self, x, y):
+        neighbors = 0
+        for i in range(-1, 2):
+            if x + i < 0 or x + i >= WIDTH:
+                continue
+            for j in range(-1, 2):
+                if y + j < 0 or y + j >= HEIGHT:
+                    continue
+                if i == 0 and j == 0:
+                    continue
+                neighbors += grid[x + i, y + j]
+        return neighbors
+
+    def update_grid(self):
+        global grid
+        new_grid = np.copy(grid)
+        for x in range(WIDTH):
+            for y in range(HEIGHT):
+                neighbors = self.count_neighbors(x, y)
+                if grid[x, y] == 1 and (neighbors not in RULE["S"]):
+                    new_grid[x, y] = 0
+                elif grid[x, y] == 0 and neighbors in RULE["B"]:
+                    new_grid[x, y] = 1
+        grid = new_grid
 
     def game_loop(self):
         if running:
@@ -186,7 +213,7 @@ class GameOfLifeApp:
         self.last_mouse_y = event.y
         self.draw_grid()
 
-    def end_pan(self):
+    def end_pan(self, event):
         self.panning = False
 
     def load_data_from_file(self):
--- grid.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-from settings import WIDTH, HEIGHT, RULE
-from board import grid
-
